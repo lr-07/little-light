e0605d640ad9b19e0279150d287c00fa6f6c298f
lr-07 <3085991323@qq.com>
2026-07-23 19:33:30 +0800

    Add lib\firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: '',
+  appId: '',
+  messagingSenderId: '',
+  projectId: '',
+  authDomain: '',
+  storageBucket: '',
+);
+
+const FirebaseOptions firebaseOptionsAndroid = FirebaseOptions(
+  apiKey: '',
+  appId: '',
+  messagingSenderId: '',
+  projectId: '',
+  authDomain: '',
+  storageBucket: '',
+);
+
+const FirebaseOptions firebaseOptionsIOS = FirebaseOptions(
+  apiKey: '',
+  appId: '',
+  messagingSenderId: '',
+  projectId: '',
+  authDomain: '',
+  storageBucket: '',
+  iosBundleId: '',
+);
